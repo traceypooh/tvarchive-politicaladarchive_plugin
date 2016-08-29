4e36a9b199e37d9461b507bf7973232a16a04f35
Dan Schultz <slifty@gmail.com>
2016-08-29 14:23:15 -0400

    Sponsor types now show pretty names

--- includes/models/class-political-ad-archive-ad.php
+++ includes/models/class-political-ad-archive-ad.php
@@ -15,6 +15,7 @@ class PoliticalAdArchiveAd {
 	private $notes;
     private $sponsors;
     private $sponsor_names;
+    private $sponsor_types;
 	private $subjects;
     private $candidates;
     private $candidate_names;
@@ -41,9 +42,8 @@ class PoliticalAdArchiveAd {
                 $this->sponsors = PoliticalAdArchiveSponsor::get_sponsors_by_acf_field_value($ad_sponsors_acf_value);
                 $this->sponsor_names = array_map(function($x) { return $x->name; }, $this->sponsors);
                 $this->sponsor_names = array_unique($this->sponsor_names);
-                $this->sponsor_types = array_map(function($x) { return $x->type; }, $this->sponsors);
-                $this->sponsor_types = array_unique($this->sponsor_types);
-                $this->sponsor_types = sizeof($this->sponsor_types) > 1?array("multiple"):$this->sponsor_types;
+                $this->sponsor_types = array_map(function($x) { return PoliticalAdArchiveAd::get_friendly_sponsor_type_name($x->type); }, $this->sponsors);
+                $this->sponsor_types = sizeof($this->sponsor_types) > 1?array("Multiple"):$this->sponsor_types;
                 $this->sponsor_affiliations = array_map(function($x) { return $x->single_ad_candidate_id; }, $this->sponsors);
                 $this->sponsor_affiliation_types = array_map(function($x) { return $x->does_support_candidate; }, $this->sponsors);
                 $ad_candidates_acf_value = array_key_exists('ad_candidates', $post_metadata)?$post_metadata['ad_candidates']:array();
@@ -81,24 +81,43 @@ class PoliticalAdArchiveAd {
 		}
 	}
 
-        public static function get_ads_with_references() {
-                global $wpdb;
-                $posts_table = $wpdb->prefix . 'posts';
-                $meta_table = $wpdb->prefix . 'postmeta';
-                $query = "SELECT DISTINCT ".$meta_table.".post_id as id
-                            FROM ".$meta_table."
-                            JOIN ".$posts_table." ON ".$meta_table.".post_id = ".$posts_table.".ID
-                           WHERE ".$meta_table.".meta_key LIKE 'references_%reference_title'
-                             AND ".$posts_table.".post_status = 'publish'";
-                $results = $wpdb->get_results($query);
+    public static function get_ads_with_references() {
+            global $wpdb;
+            $posts_table = $wpdb->prefix . 'posts';
+            $meta_table = $wpdb->prefix . 'postmeta';
+            $query = "SELECT DISTINCT ".$meta_table.".post_id as id
+                        FROM ".$meta_table."
+                        JOIN ".$posts_table." ON ".$meta_table.".post_id = ".$posts_table.".ID
+                       WHERE ".$meta_table.".meta_key LIKE 'references_%reference_title'
+                         AND ".$posts_table.".post_status = 'publish'";
+            $results = $wpdb->get_results($query);
 
-                // Package the results
-                $ads = array();
-                foreach($results as $result) {
-                        $ad = new PoliticalAdArchiveAd($result->id);
-                        $ads[] = $ad;
-                }
+            // Package the results
+            $ads = array();
+            foreach($results as $result) {
+                    $ad = new PoliticalAdArchiveAd($result->id);
+                    $ads[] = $ad;
+            }
 
-                return $ads;
+            return $ads;
+    }
+
+    public static function get_friendly_sponsor_type_name($sponsor_type) {
+        switch(strtolower($sponsor_type)) {
+            case "candcmte":
+                return "Candidate Committee";
+            case "superpac":
+                return "Super PAC";
+            case "pac":
+                return "PAC";
+            case "501c4":
+                return "Non Profit";
+            case "501c6":
+                return "Trade Association";
+            case "carey":
+                return "Hybrid Super PAC";
+            case "527":
+                return "527";
         }
+    }
 }
